96f436fa553e21e063dd73c75a175828af5645b0
Jacob B West <jacob.b.west@mines.sdmt.edu>
2021-06-07 23:15:37 -0500

    A few more changes to the navbar

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,16 +1,15 @@
 <template>
-  <nav>
-    <div class="logoBanner">
-      <img class="logo" alt="Black Hills Gear Logo" src="/logoBadge.png" />
-      <h1 class="logoText">Black Hills Gear</h1>
-    </div>
-    <div class="navButtons">
-      <v-btn plain>Home</v-btn>
-      <v-btn plain>Store</v-btn>
-      <v-btn plain>About</v-btn>
-      <v-btn plain>Contact</v-btn>
-    </div>
-  </nav>
+  <div class="backgroundImage">
+    <nav>
+      <img class="logo" alt="Black Hills Gear Logo" src="/BHG-Logo_White.svg" />
+      <div class="navButtons">
+        <v-btn plain>Home</v-btn>
+        <v-btn plain>Store</v-btn>
+        <v-btn plain>About</v-btn>
+        <v-btn plain>Contact</v-btn>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -22,18 +21,24 @@ export default {}
 
 nav {
   font-family: 'Playfair Display', serif;
-  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url('/custerstatepark.jpg');
-  background-position: center;
-  background-size: 100%;
   display: flex;
-  justify-content: space-between;
-  height: 32vh;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 24vh;
   width: 100vw;
 }
 
 .navButtons {
   margin: 2.5em;
+}
+
+.backgroundImage {
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url('/custerstatepark.jpg');
+  background-position: center;
+  background-size: 100%;
+  height: 24vh;
+  width: 100vw;
 }
 
 .logoBanner {
@@ -44,7 +49,7 @@ nav {
 }
 
 .logo {
-  max-height: 6vh;
+  max-height: 16vh;
   margin: 2em;
 }
 
