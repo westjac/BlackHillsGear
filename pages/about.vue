a63fab5d03743e58d2f33b39940379805e3cf13e
Jacob B West <jacob.b.west@mines.sdmt.edu>
2021-06-20 20:07:14 -0500

    Added the about page

---
+++ pages/about.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container mt-6>
+    <v-row justify="center">
+      <v-col cols="7">
+        <h1 class="title">About Black Hills Gear</h1>
+        <br />
+        <p>
+          Black Hills Gear is US Veteran owned company that fabricates
+          Silencer/Suppressor MAD (Mirage Abatement Device) wraps which mitigate
+          heat mirage thus enabling proper target acquisition. Suppressors can
+          generate extreme heat temperatures while on the range, hunting with
+          thermal night vision, or at say a long range competition. This
+          endeavor started with using other covers on the market that wouldn’t
+          stay in place and charred away after minimal use. We test and evaluate
+          our gear and always welcome feedback from our much appreciated
+          customers. All Black Hills Gear is USA made and sources all materials
+          from US suppliers.
+        </p>
+      </v-col>
+      <v-col cols="4">
+        <v-img height="100%" src="./images/BHG_Hat.jpg"> </v-img>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="4">
+        <v-img height="100%" src="./images/paul.jpg"> </v-img>
+      </v-col>
+      <v-col cols="7">
+        <h1 class="title">About The Owner</h1>
+        <br />
+        <p>
+          My Name is Paul Sheets, and I recently retired from 9 yrs. crane
+          operating after 25 year USAF Parachute Rigger/Survival Equipment
+          Specialist career. Learning a vast knowledge of various fabric
+          textiles, fabrication and repairs on various life-sustaining equipment
+          required for aircrew personnel, I decided to use my experience with
+          the different textiles as a rigger in the Survival Equipment arena has
+          enabled the attention to detail and God willing started small company
+          Black Hills Gear, with a goal of providing a top quality product to
+          the shooting community. I am an avid outdoorsman that loves the Lord
+          and all His creations, family, and our Constitutional Rights! Thanks
+          to all those that served and are serving whether in Military, Law
+          Enforcement, or Fire Fighters!
+        </p>
+        <p style="text-align: center; font-size: 24px">God Bless the USA!!!!</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
