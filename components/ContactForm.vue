294ab0081f4ffbcdcc78234d360dbf2fc973047c
Jacob B West <jacob.b.west@mines.sdmt.edu>
2021-06-20 20:07:32 -0500

    Added the contact page

---
+++ components/ContactForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-form
+    name="contactUs"
+    ref="form"
+    v-model="valid"
+    method="POST"
+    data-netlify="true"
+  >
+    <input type="hidden" name="form-name" value="contactUs" />
+    <v-text-field solo v-model="name" name="Name" label="Name" required></v-text-field>
+    <v-text-field solo v-model="email" name="Email" label="Email" required></v-text-field>
+    <v-text-field
+      solo
+      v-model="cellPhone"
+      name="Cell Phone"
+      label="Cell Phone"
+      required
+    ></v-text-field>
+    <v-textarea solo name="Message" label="Message" v-model="message"></v-textarea>
+    <v-btn class="mr-4" type="submit" :disabled="false"> submit </v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    name: '',
+    email: '',
+    cellPhone: '',
+    message: '',
+  }),
+}
+</script>
---
+++ pages/contact.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row style="text-align: center">
+      <v-col>
+        <h1 class="header">Get in touch with us.</h1>
+        <br />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <ContactForm />
+      </v-col>
+      <v-col>
+        <v-img src="BHG-Logo_White.svg"></v-img>
+        <br />
+        <p class="contactText">
+          Questions? Comments? Want to know more about our product? Shoot us a
+          message and we will get back with you as fast as you can say "got
+          'em!"
+        </p></v-col
+      >
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+.contactText {
+  font-size: 20px;
+  text-align: center;
+}
+</style>
