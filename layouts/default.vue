27c14d33268aad9455de0a04c8e2fd8fa14b8c5e
Jacob B. West <westjacob1@gmail.com>
2023-03-13 16:59:34 -0600

    Updated top text

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,10 +3,7 @@
     <Navigation />
     <v-main>
       <v-alert dense outlined prominent text dismissible type="warning"
-        >Begining January 25th no custom orders will be processed until April.
-        Although, we do have some various colors and sizes available for
-        immediate shipping during this time frame. Please contact us for more
-        information. Sorry for the inconvenience.
+        >Currently away and will process orders in the first week in April. Thanks
       </v-alert>
       <nuxt id="siteContent" />
     </v-main>
